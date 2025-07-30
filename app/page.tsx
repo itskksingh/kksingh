e94d91105862c3aafad3b14e6e60ef2ba1de22fd
K K Singh <realkishanpyare@gmail.com>
2025-07-30 16:51:18 +0530

    skills card responsive made

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,10 @@
 import { FaReact } from "react-icons/fa";
+import Expertise from "../components/Expertise.jsx";
 import TechStack from "../components/Techstack.jsx";
-
-const techStacks = [
-  { category: "Front-End", skills: "HTML, CSS, JavaScript, React, Angular" },
-  { category: "Back-End", skills: "Node.js, Express, Python, Django" },
-  { category: "Databases", skills: "MySQL, PostgreSQL, MongoDB" },
-  { category: "Tools & Platforms", skills: "Git, Docker, AWS, Heroku" },
-  { category: "Others", skills: "RESTful APIs, GraphQL" },
-];
 
 const Homepage = () => {
   return (
-    <div className='w-full max-w-7xl mx-auto p-8'>
+    <div className='w-full max-w-7xl mx-auto p-4 lg:p-8'>
       <div className='border border-[#3b413d] rounded-lg w-full'>
         <div className='bg-[#272730] relative flex flex-col items-center justify-center w-full py-10 rounded-lg borderbox-animated z-20 overflow-hidden'>
           <div className='text-center'>
@@ -21,19 +14,12 @@ const Homepage = () => {
 
           <div className='flex flex-col lg:flex-row divide-[#525259] divide-x-1 lg:mt-10  w-full'>
             {/* Left: TechStack */}
-            <div className='flex-1 px-15 flex items-center overflow-hidden w-full'>
+            <div className='flex-1 px-5 lg:px-15 flex items-center overflow-hidden w-full'>
               <TechStack />
             </div>
             {/* Right: Custom content */}
-            <div className='flex-1 flex flex-col items-start justify-center p-10 w-full'>
-              <ul className='space-y-5 font-mono text-base text-[#8f8f92] list-disc '>
-                {techStacks.map((item) => (
-                  <li key={item.category}>
-                    <span className='text-white font-bold block md:inline'>{item.category}:</span>
-                    <span className='ml-2'>{item.skills}</span>
-                  </li>
-                ))}
-              </ul>
+            <div className='flex-1 flex flex-col items-center justify-center p-10 pb-5 w-full'>
+              <Expertise />
             </div>
           </div>
 
---
+++ components/Expertise.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+const techStacks = [
+  { category: "Front-End", skills: "HTML, CSS, JavaScript, React, Angular" },
+  { category: "Back-End", skills: "Node.js, Express, Python, Django" },
+  { category: "Databases", skills: "MySQL, PostgreSQL, MongoDB" },
+  { category: "Tools & Platforms", skills: "Git, Docker, AWS, Heroku" },
+  { category: "Others", skills: "RESTful APIs, GraphQL" },
+];
+
+const Expertise = () => {
+  return (
+    <ul className='space-y-5 font-mono text-base text-[#8f8f92] list-disc '>
+      {techStacks.map((item) => (
+        <li key={item.category}>
+          <span className='text-white font-bold block md:inline'>{item.category}:</span>
+          <span className='ml-2'>{item.skills}</span>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default Expertise;
