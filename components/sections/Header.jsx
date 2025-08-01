8d4f6d2b0cbf3d01bcaaa6b8e531199d2655d212
K K Singh <realkishanpyare@gmail.com>
2025-08-01 20:28:27 +0530

    Header fixed

--- components/sections/Header.jsx
+++ components/sections/Header.jsx
@@ -3,21 +3,21 @@ import { FiMenu } from "react-icons/fi";
 
 const Header = () => {
   return (
-    <header className="w-full flex items-center justify-between max-w-7xl rounded-lg bg-[#272730] border border-zinc-600 py-5 px-5">
-      <span className="flex items-center gap-3">
-        <div className="relative w-[50px] h-[50px]">
+    <header className='w-full flex items-center justify-between max-w-7xl rounded-lg bg-[#272730] border border-zinc-600 py-3 lg:py-5 px-5'>
+      <span className='flex items-center gap-3'>
+        <div className='relative w-[50px] h-[50px]'>
           <Image
-            src="/img/kklogo.png"
-            alt="kksingh-nextjs-developer-logo"
+            src='/img/kklogo.png'
+            alt='kksingh-nextjs-developer-logo'
             fill
             priority
-            sizes="50px"
-            className="object-contain"
+            sizes='50px'
+            className='object-contain'
           />
         </div>
-        <span className="logo-name text-2xl font-mono font-semibold">kksingh.dev</span>
+        <span className='logo-name text-xl lg:text-2xl font-mono font-semibold'>kksingh.dev</span>
       </span>
-      <FiMenu className="text-zinc-200 text-4xl border border-zinc-400 p-1.5 rounded self-center cursor-pointer" />
+      <FiMenu className='text-zinc-200 text-4xl border border-zinc-400 p-1.5 rounded self-center cursor-pointer' />
     </header>
   );
 };
