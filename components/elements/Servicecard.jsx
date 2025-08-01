bab805b52325d4797f41e12723174fbf64dcb0d0
K K Singh <realkishanpyare@gmail.com>
2025-08-01 14:54:10 +0530

    service padding fixed

--- components/elements/Servicecard.jsx
+++ components/elements/Servicecard.jsx
@@ -1,7 +1,7 @@
 const Servicecard = ({ icon: Icon, title, content, className = "" }) => (
   <div
     className={
-      "flex w-full flex-col items-start justify-center group border border-zinc-700 rounded-lg px-6 py-10 space-y-3 transition-all duration-300 hover:shadow-lg hover:border-lime-400  " +
+      "flex w-full flex-col items-start justify-center group border border-zinc-700 rounded-lg px-6 py-10 gap-3 transition-all duration-300 hover:shadow-lg hover:border-lime-400  " +
       className
     }
     tabIndex={0}
@@ -10,7 +10,7 @@ const Servicecard = ({ icon: Icon, title, content, className = "" }) => (
       <Icon className='text-white text-4xl group-hover:text-lime-400' />
     </span>
     <h3 className='font-mono text-xl font-semibold text-white'>{title}</h3>
-    <p className='service-para text-gray-300'>{content}</p>
+    <p className='service-para text-gray-300 text-left'>{content}</p>
   </div>
 );
 
